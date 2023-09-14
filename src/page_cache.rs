9f02f8ec7fb2c75332571bdae4984519d22bd950
Subhan <s.ashraf9@protonmail.com>
2023-09-14 20:55:12 +0100

    attempt at unpin on drop

--- src/page_cache.rs
+++ src/page_cache.rs
@@ -11,13 +11,9 @@ use tokio::sync::{Mutex, RwLock};
 use crate::{
     disk::Disk,
     page::{Page, PageId},
-    replacer::{AccessType, LRUKReplacer},
+    replacer::{AccessType, LRUKHandle},
 };
 
-#[cfg(not(test))]
-pub const CACHE_SIZE: usize = 8;
-
-#[cfg(test)]
 pub const CACHE_SIZE: usize = 8;
 
 pub type FrameId = usize;
@@ -62,21 +58,39 @@ impl FreeList {
     }
 }
 
+pub struct Pin<'a> {
+    pub page: &'a Page,
+    i: FrameId,
+    replacer: LRUKHandle,
+}
+
+impl<'a> Drop for Pin<'a> {
+    fn drop(&mut self) {
+        self.replacer.unpin(self.i);
+    }
+}
+
+impl<'a> Pin<'a> {
+    pub fn new(page: &'a Page, i: FrameId, replacer: LRUKHandle) -> Self {
+        Self { page, i, replacer }
+    }
+}
+
 #[derive(Clone)]
 pub struct PageCache(Arc<PageCacheInner>);
 
 impl PageCache {
-    pub fn new(disk: Disk, replacer: LRUKReplacer, next_page_id: PageId) -> Self {
+    pub fn new(disk: Disk, replacer: LRUKHandle, next_page_id: PageId) -> Self {
         let inner = Arc::new(PageCacheInner::new(disk, replacer, next_page_id));
 
         Self(inner)
     }
 
-    pub async fn new_page<'a>(&self) -> Option<&Page> {
+    pub async fn new_page<'a>(&self) -> Option<Pin> {
         self.0.new_page().await
     }
 
-    pub async fn fetch_page<'a>(&self, page_id: PageId) -> Option<&Page> {
+    pub async fn fetch_page<'a>(&self, page_id: PageId) -> Option<Pin> {
         self.0.fetch_page(page_id).await
     }
 
@@ -99,15 +113,14 @@ struct PageCacheInner {
     free: Mutex<FreeList>,
     disk: Disk,
     next_page_id: AtomicI32,
-    replacer: Mutex<LRUKReplacer>,
+    replacer: LRUKHandle,
 }
 
 impl PageCacheInner {
-    pub fn new(disk: Disk, replacer: LRUKReplacer, next_page_id: PageId) -> Self {
+    pub fn new(disk: Disk, replacer: LRUKHandle, next_page_id: PageId) -> Self {
         let pages = std::array::from_fn(|_| Page::default());
         let page_table = RwLock::new(HashMap::new());
         let free = Mutex::new(FreeList::new());
-        let replacer = Mutex::new(replacer);
         let next_page_id = AtomicI32::new(next_page_id);
 
         Self {
@@ -124,36 +137,34 @@ impl PageCacheInner {
         self.next_page_id.fetch_add(1, SeqCst)
     }
 
-    pub async fn new_page<'a>(&self) -> Option<&Page> {
+    pub async fn new_page<'a>(&self) -> Option<Pin> {
         let page_id = self.allocate_page();
 
         self.try_get_page(page_id).await
     }
 
-    pub async fn fetch_page<'a>(&self, page_id: PageId) -> Option<&Page> {
+    pub async fn fetch_page<'a>(&self, page_id: PageId) -> Option<Pin> {
         if let Some(i) = self.page_table.read().await.get(&page_id) {
-            let mut replacer = self.replacer.lock().await;
-            replacer.record_access(*i, &AccessType::Get);
-            replacer.pin(*i);
+            self.replacer.record_access(*i, AccessType::Get);
+            self.replacer.pin(*i);
 
-            return Some(&self.pages[*i]);
+            return Some(Pin::new(&self.pages[*i], *i, self.replacer.clone()));
         };
 
         self.try_get_page(page_id).await
     }
 
-    async fn try_get_page(&self, page_id: PageId) -> Option<&Page> {
+    async fn try_get_page(&self, page_id: PageId) -> Option<Pin> {
         // TODO: avoid attempt to acquire lock on empty free list
-        let mut replacer = self.replacer.lock().await;
         let i = match self.free.lock().await.pop() {
             Some(i) => i,
-            None => replacer.evict()?,
+            None => self.replacer.evict().await?,
         };
 
         let mut page_w = self.pages[i].write().await;
-        replacer.remove(i);
-        replacer.record_access(i, &AccessType::Get);
-        replacer.pin(i);
+        self.replacer.remove(i);
+        self.replacer.record_access(i, AccessType::Get);
+        self.replacer.pin(i);
 
         if page_w.dirty {
             self.disk.write_page(page_w.id, &page_w.data);
@@ -168,13 +179,13 @@ impl PageCacheInner {
         page_w.id = page_id;
         page_w.data = data;
 
-        Some(&self.pages[i])
+        Some(Pin::new(&self.pages[i], i, self.replacer.clone()))
     }
 
     pub async fn unpin_page(&self, page_id: PageId) {
         let page_table = self.page_table.read().await;
         let Some(i) = page_table.get(&page_id) else { return };
-        self.replacer.lock().await.unpin(*i);
+        self.replacer.unpin(*i);
     }
 
     pub async fn flush_page(&self, page_id: PageId) {
@@ -198,65 +209,67 @@ impl PageCacheInner {
 mod test {
     use std::io;
 
-    use crate::{disk::Disk, page_cache::PageCache, replacer::LRUKReplacer, test::CleanUp};
+    use crate::{disk::Disk, page_cache::PageCache, replacer::LRUKHandle, test::CleanUp};
 
     #[tokio::test]
-    #[ignore]
     async fn test_pm_replacer() -> io::Result<()> {
         const DB_FILE: &str = "./test_pm_replacer.db";
         let _cu = CleanUp::file(DB_FILE);
         let disk = Disk::new(DB_FILE).await?;
 
-        let replacer = LRUKReplacer::new(2);
+        // let replacer = LRUKReplacer::new(2);
+        let replacer = LRUKHandle::new(2);
         let pc: PageCache = PageCache::new(disk, replacer, 0);
 
-        let _page_0 = pc.new_page().await.expect("should return page 0"); // id = 0 ts = 0
-        let _page_1 = pc.new_page().await.expect("should return page 1"); // id = 1 ts = 1
-        let _page_2 = pc.new_page().await.expect("should return page 2"); // id = 2 ts = 2
+        {
+            let _page_0 = pc.new_page().await.expect("should return page 0"); // id = 0 ts = 0
+            let _page_1 = pc.new_page().await.expect("should return page 1"); // id = 1 ts = 1
+            let _page_2 = pc.new_page().await.expect("should return page 2"); // id = 2 ts = 2
 
-        let inner = pc.0.clone();
-        let page_table = inner.page_table.read().await;
-        assert!(inner.free.lock().await.is_empty());
-        assert!(page_table.len() == 3);
-        assert!(page_table.contains_key(&2));
-        assert!(page_table.contains_key(&1));
-        assert!(page_table.contains_key(&0));
-        drop(page_table);
-        drop(inner);
+            let inner = pc.0.clone();
+            let page_table = inner.page_table.read().await;
+            assert!(inner.free.lock().await.is_empty());
+            assert!(page_table.len() == 3);
+            assert!(page_table.contains_key(&2));
+            assert!(page_table.contains_key(&1));
+            assert!(page_table.contains_key(&0));
+            drop(page_table);
+            drop(inner);
 
-        pc.fetch_page(0).await; // ts = 3
-        pc.fetch_page(0).await; // ts = 4
+            pc.fetch_page(0).await; // ts = 3
+            pc.fetch_page(0).await; // ts = 4
 
-        pc.fetch_page(1).await; // ts = 5
+            pc.fetch_page(1).await; // ts = 5
 
-        pc.fetch_page(0).await; // ts = 6
-        pc.fetch_page(0).await; // ts = 7
+            pc.fetch_page(0).await; // ts = 6
+            pc.fetch_page(0).await; // ts = 7
 
-        pc.fetch_page(1).await; // ts = 8
+            pc.fetch_page(1).await; // ts = 8
 
-        pc.fetch_page(2).await; // ts = 9
+            pc.fetch_page(2).await; // ts = 9
+        }
 
         // Page 2 is the least regularly accessed and should have the largest k distance of 7
         // Page 1 should have a k distance of 3
         // Page 0 should have a k distance of 1
 
         // Unpin pages so they can be evicted:
-        pc.unpin_page(0).await;
-        pc.unpin_page(0).await;
-        pc.unpin_page(0).await;
-        pc.unpin_page(0).await;
-        pc.unpin_page(0).await;
+        // pc.unpin_page(0).await;
+        // pc.unpin_page(0).await;
+        // pc.unpin_page(0).await;
+        // pc.unpin_page(0).await;
+        // pc.unpin_page(0).await;
 
-        pc.unpin_page(1).await;
-        pc.unpin_page(1).await;
-        pc.unpin_page(1).await;
+        // pc.unpin_page(1).await;
+        // pc.unpin_page(1).await;
+        // pc.unpin_page(1).await;
 
-        pc.unpin_page(2).await;
-        pc.unpin_page(2).await;
+        // pc.unpin_page(2).await;
+        // pc.unpin_page(2).await;
 
         let _page_3 = pc.new_page().await.expect("should return page 3");
 
-        let inner = pc.0;
+        let inner = &pc.0;
         let page_table = inner.page_table.read().await;
         assert!(page_table.len() == 3);
         assert!(page_table.contains_key(&3));
